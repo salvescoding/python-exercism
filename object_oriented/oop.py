4df46fe5e7d2bae31803e7b02eafae41eb366810
Sergio Alves <salves2383@gmail.com>
2018-06-24 17:02:49 +0200

    practicing object oriented programming

--- object_oriented/oop.py
+++ object_oriented/oop.py
@@ -1,9 +1,29 @@
-class MyClass(object):
-  pass
+class MaxSizeList:
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.new_list = []
+
+    def push(self, arg):
+        self.new_list.append(arg)
+        if len(self.new_list) > self.max_size:
+            self.new_list.pop(0)
+
+    def get_list(self):
+          return self.new_list
 
 
-new_c = MyClass()
-print(new_c)
-print()
-print(type(new_c))
+a = MaxSizeList(3)
+b = MaxSizeList(2)
 
+a.push("hello")
+a.push("hi")
+a.push("let's")
+a.push("go")
+
+b.push("hello")
+b.push("hi")
+b.push("let's")
+b.push("go")
+
+print(a.get_list())
+print(b.get_list())
