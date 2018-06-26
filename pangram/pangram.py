79e017e9f109eaa8f241542609729cf6572ee7f4
Sergio Alves <salves2383@gmail.com>
2018-06-26 15:57:14 +0200

    pangram solution kata

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,6 @@
+import string
+
 def is_pangram(sentence):
-    pass
+    ascii_all = string.ascii_lowercase
+    sentence = sentence.lower()
+    return all(letter in sentence for letter in ascii_all)
