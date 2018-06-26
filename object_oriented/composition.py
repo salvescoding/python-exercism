5738107ffae02697998a19c666c2e064a6e79081
Sergio Alves <salves2383@gmail.com>
2018-06-26 13:57:06 +0200

    Implemented simple composition with classes

--- object_oriented/composition.py
+++ object_oriented/composition.py
@@ -1,50 +1,40 @@
 from datetime import datetime
-import abc
+
+
+class CSVFormatter(object):
+
+    def __init__(self):
+      self.delim = ','
+
+    def formatter(self,elements):
+        return self.delim.join(elements)
+
+class LogFormatter(object):
+
+    def formatter(self,text):
+        dt = datetime.now()
+        date_strf = dt.strftime('%D-%M-%Y %H:%M')
+        return "{}:  {}".format(date_strf, text)
 
 
 class WriteFile(object):
 
-    __metaclass__ = abc.ABCMeta
-
-    @abc.abstractmethod
-    def write(self):
-        return
-
-    def __init__(self, file_name):
+    def __init__(self, file_name, formatter):
         self.file_name = file_name
-
-    def write_line(self, text):
-        with open(self.file_name, 'a') as f:
-            f.write(text + '\n')
-
-
-class LogFile(WriteFile):
-
-    def __init__(self, file_name):
-        super().__init__(file_name)
+        self.formatter = formatter()
 
     def write(self, text):
-        dt = datetime.now()
-        date_strf = dt.strftime('%D-%M-%Y %H:%M')
-        self.write_line('{}  {}'.format(date_strf, text))
+        line = self.formatter.formatter(text)
+        with open(self.file_name, 'a') as f:
+            f.write(line + '\n')
 
 
-class DelimFile(WriteFile):
-
-    def __init__(self, file_name, delimeter):
-        super().__init__(file_name)
-        self.delim = delimeter
-
-    def write(self, elements):
-        line = self.delim.join(elements)
-        self.write_line(line)
+writecsv = WriteFile('text2.csv', CSVFormatter)
+writelog = WriteFile('logo2.text', LogFormatter)
 
 
-log = LogFile('logo.txt')
-delim = DelimFile('text.csv', ',')
+writecsv.write(['1', '2', '3', '4'])
+writelog.write("This is the first log")
 
-log.write('This is the first log')
-log.write('This is the second log')
-
-delim.write(['a', 'b', 'c', 'd'])
-delim.write(['1', '2', '3', '4'])
+writecsv.write(['1', 'a,c', '3', '4'])
+writelog.write("This is the second log")
