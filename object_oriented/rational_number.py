2cfd4746b7fbbbfc86721850fb082d9e75e42b52
Sergio Alves <salves2383@gmail.com>
2018-07-04 22:09:32 +0200

    Working the fractions and understand rational vs irrational numbers

---
+++ object_oriented/rational_number.py
@@ -0,0 +1,18 @@
+from fractions import Fraction
+import math
+
+print(Fraction(0.125))
+# should be 1/8
+
+print(Fraction('0.125'))
+# python recognizes is a fraction and transforms it to 1/8
+
+x = Fraction(math.pi)
+
+print("Pi respresentation in a fraction {0}".format(x))
+print("The aproximations of pi in a float is {0}".format(float(x)))
+
+
+# We can limit the denomitar by using the following function
+print(x.limit_denominator(10))
+print("The float would be {0}".format(float(x.limit_denominator(10))))
