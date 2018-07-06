c469ec33590322c0b48f09a38cb6d048f5de650a
Sergio Alves <salves2383@gmail.com>
2018-07-06 13:37:15 +0200

    Implemented kata to calculate bills for roommates that share a flat

---
+++ calculate-bills/calculate.py
@@ -0,0 +1,53 @@
+room_mates = {
+    'Sergio': 60,
+    'Marise': 60,
+    'Erika': 35,
+    'Floriane': 25
+}
+totals = {
+    'Sergio': 0,
+    'Marise': 0,
+    'Erika': 0,
+    'Floriane': 0
+}
+
+days = 60
+bill = 77.32
+
+
+def bill_per_day():
+    """ Calculate bill per day """
+    return round(bill / 60, 3)
+
+def calculate_value_to_add(days_to_calc):
+    """ Returns the value to add on each roommate on the days to be calculated.
+
+        Arguments: Days to calculate
+    """
+    return round((bill_per_day() * days_to_calc) / len(room_mates), 2)
+
+
+def get_roommate_with_lowest_days():
+    """ Returns a tuple with the key and value, with the lowest value from the dict """
+    return min(room_mates.items(), key=lambda x: x[1])
+
+
+def remove_roommate_from_dict(roommate):
+    """ Removes the roommate from the original dictionary, returns None if not found.
+
+        Arguments: The roommate as a string.
+    """
+    room_mates.pop(roommate, None)
+
+
+while days > 0:
+    person, days_to_calc = get_roommate_with_lowest_days()
+    value_to_add = calculate_value_to_add(days_to_calc)
+    for key, value in room_mates.items():
+        totals[key] += value_to_add
+        room_mates[key] -= days_to_calc
+    days -= days_to_calc
+    remove_roommate_from_dict(person)
+
+print(totals)
+
