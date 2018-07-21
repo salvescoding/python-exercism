6e0ca05422e4ad6ee0c07017144d457547e1d09d
Sergio Alves <salves2383@gmail.com>
2018-07-21 18:07:51 +0200

    Refactor kata bob

--- bob/bob.py
+++ bob/bob.py
@@ -1,10 +1,11 @@
 def hey(phrase):
     phrase = phrase.strip()
-    if phrase.isupper():
-        return "Whoa, chill out!" if not phrase[-1:] == '?' else "Calm down, I know what I'm doing!"
-    elif phrase[-1:] == '?':
-        return "Sure."
-    elif phrase == '':
+    if not phrase:
         return 'Fine. Be that way!'
-    else:
-        return 'Whatever.'
+    elif phrase.isupper():
+        if not phrase.endswith('?'):
+            return "Whoa, chill out!"
+        return "Calm down, I know what I'm doing!"
+    elif phrase.endswith('?'):
+        return "Sure."
+    return 'Whatever.'
