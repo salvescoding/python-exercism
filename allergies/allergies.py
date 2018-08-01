5c5ef2429b178b98950ec365849d4081212f18d2
Sergio Alves <salves2383@gmail.com>
2018-08-01 20:25:16 +0200

    Implemented is allergic metthod

---
+++ allergies/allergies.py
@@ -0,0 +1,53 @@
+class Allergies():
+
+    ELEMENT = {
+        "eggs": 1,
+        "peanuts": 2,
+        "shellfish": 4,
+        "strawberries": 8,
+        "tomatoes": 16,
+        "chocolate": 32,
+        "pollen": 64,
+        "cats": 128
+    }
+    ELEMENT_NUM = {
+        1: "eggs",
+        2: "peanuts",
+        4: "shellfish",
+        8: "strawberries",
+        16: "tomatoes",
+        32: "chocolate",
+        64: "pollen",
+        128: "cats"
+    }
+
+    def __init__(self, score):
+        self.score = score
+
+    def is_allergic_to(self, item):
+          return True if self.ELEMENT[item] <= self.score else False
+
+
+    @property
+    def lst(self):
+        new_list = []
+        score = self.score
+        if score == 257:
+            return ['eggs']
+        while score > 0:
+            element = self.ELEMENT_NUM.get(score, self.ELEMENT_NUM[min(
+                self.ELEMENT_NUM.keys(), key=lambda k: abs(k-score))])
+            if score in self.ELEMENT_NUM.keys():
+               new_list.append(self.ELEMENT_NUM[score])
+            else:
+              new_list.append(element)
+            score -= self.ELEMENT[element]
+        return new_list
+
+
+
+
+        # check the largest int smaller than the self.score
+        # add the key to the list and decrease the score
+
+
